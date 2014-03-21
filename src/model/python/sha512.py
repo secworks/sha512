1f199211fa86daf41060dc44fa1a81a5ff86603e
Joachim Strömbergson <joachim@secworks.se>
2014-03-21 19:35:24 +0100

    Adding H constants for the different modes.

--- src/model/python/sha512.py
+++ src/model/python/sha512.py
@@ -49,6 +49,11 @@ import sys
 #-------------------------------------------------------------------
 # Constants.
 #-------------------------------------------------------------------
+# Hash modes supported.
+MODE_SHA_512_224 = 0
+MODE_SHA_512_256 = 1
+MODE_SHA_284     = 2
+MODE_SHA_512     = 3
 
 
 #-------------------------------------------------------------------
@@ -89,9 +94,33 @@ class SHA512():
                   0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
         
         
-    def init(self):
-        self.H = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-                  0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]
+    def init(self, mode):
+        if mode == MODE_SHA_512_224:
+            self.H = [0x8c3d37c819544da2, 0x73e1996689dcd4d6,
+                      0x1dfab7ae32ff9c82, 0x679dd514582f9fcf,
+                      0x0f6d2b697bd44da8, 0x77e36f7304c48942,
+                      0x3f9d85a86a1d36c8, 0x1112e6ad91d692a1]
+
+        elif mode == MODE_SHA_512_256:
+            self.H = [0x22312194fc2bf72c, 0x9f555fa3c84c64c2, 
+                      0x2393b86b6f53b151, 0x963877195940eabd, 
+                      0x96283ee2a88effe3, 0xbe5e1e2553863992, 
+                      0x2b0199fc2c85b8aa, 0x0eb72ddc81c52ca2]
+                      
+        elif mode == MODE_SHA_384:
+            self.H = [0xcbbb9d5dc1059ed8, 0x629a292a367cd507,
+                      0x9159015a3070dd17, 0x152fecd8f70e5939, 
+                      0x67332667ffc00b31, 0x8eb44a8768581511, 
+                      0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4]
+
+        elif mode == MODE_SHA_512:
+            self.H = [0x6a09e667f3bcc908, 0xbb67ae8584caa73b,
+                      0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 
+                      0x510e527fade682d1, 0x9b05688c2b3e6c1f, 
+                      0x1f83d9abfb41bd6b, 0x5be0cd19137e2179]
+
+        else:
+            print "Unknown mode!"
         
 
     def next(self, block):
