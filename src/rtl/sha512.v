1ffbff7d66d1089eb67cd277f22ad23c1426ed71
Joachim Strömbergson <joachim@secworks.se>
2014-09-11 17:40:07 +0200

    Changing to asynch reset.

--- src/rtl/sha512.v
+++ src/rtl/sha512.v
@@ -278,10 +278,10 @@ module sha512(
   // reg_update
   //
   // Update functionality for all registers in the core.
-  // All registers are positive edge triggered with synchronous
+  // All registers are positive edge triggered with asynchronous
   // active low reset. All registers have write enable.
   //----------------------------------------------------------------
-  always @ (posedge clk)
+  always @ (posedge clk or negedge reset_n)
     begin
       if (!reset_n)
         begin
